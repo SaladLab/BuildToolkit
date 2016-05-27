95e6b6362fcf376e58f5f9c7786294423701c729
Esun Kim <veblush@gmail.com>
2016-05-27 17:20:11 +0900

    Use UniGet 0.3.1

--- BuildScript/BuildLib/Unity.fs
+++ BuildScript/BuildLib/Unity.fs
@@ -7,7 +7,7 @@ module Unity =
     open System.IO
     
     let unityExe = lazy ("C:/Program Files/Unity/Editor/Unity.exe") // TODO: discovery
-    let uniGet = lazy ((getNugetPackage "UniGet" "0.3.0") @@ "tools" @@ "UniGet.exe")
+    let uniGet = lazy ((getNugetPackage "UniGet" "0.3.1") @@ "tools" @@ "UniGet.exe")
 
     let unity projectPath args = 
         let result = 
